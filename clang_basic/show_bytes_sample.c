405b65f6371eeae903fcded867aeb57cd961c5c1
r00tyy <82049406@qq.com>
2016-05-12 19:39:27 +0800

    show bytes

---
+++ clang_basic/show_bytes_sample.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef unsigned char *byte_pointer;    // byte_pointer 就是等于 unsigned char
+
+void show_bytes(byte_pointer start, int len )
+{
+  int i;
+  for (i = 0; i < len; i++) {
+    printf("%.2x\n", start[i]);
+  }
+  printf("\n");
+}
+
+void show_int(int x) {
+  show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x) {
+  show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_bytes(void *x) {
+  show_bytes((byte_pointer)  &x, sizeof(void *));
+}
