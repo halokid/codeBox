93d0472e8c052a032ec58294616ad8d0d16edc77
82049406@qq.com <82049406@qq.com>
2016-07-14 11:01:00 +0100

    0x style

---
+++ clang_basic/0x_style.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	printf("int %d\n", 0xBA);
+	//printf("string %s\n", 0xBA);
+
+	char *ptr;
+	ptr = malloc(1000);
+	memset(ptr, 0xBA, 500);
+	printf("int ptr %d\n", ptr);
+	printf("string ptr %s\n", ptr);
+
+}
