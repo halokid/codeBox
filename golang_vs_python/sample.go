799926a3457f66c5247634e6fa7b95ea50bfb564
r00txx <82049406@qq.com>
2017-05-05 11:00:41 +0800

    V2

--- golang_vs_python/sample.go
+++ golang_vs_python/sample.go
@@ -322,6 +322,114 @@ func main() {
 
 
 
+----------------- ºï×Ó²¹¶¡ -------------------------
+
+#python version
+import urllib
+
+def say_hi(usr):
+  if auth(usr):
+    print 'Hi, %s' % usr
+  else
+    print 'unknow user %s' % usr
+    
+    
+def auth(usr):
+  try:
+    auth_url = 'localhost'
+    r = urllib.urlopen(auth_url + '/' + usr)
+    return r.getcode() == 200
+  except:
+    return false;
+
+
+def sayhitest():
+  #Test authenticated user
+  globals()['auth'] = lambda x: True
+  say_hi('John')
+  
+  #Test unauthenticated user
+  globals()['auth'] = lambda x: False
+  say_hi('john')
+  
+if __name__ == '__main__':
+  sayhitest()
+
+    
+    
+    
+
+#golang version
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+func sayHi(user string) {
+  if !auth(user) {
+    fmt.Println("unknow user %v\n", user)
+    return
+  }
+  fmt.Println("Hi, %v\n", user)
+}
+
+
+var auth = func(user string) bool {
+  authURL := "localhost"
+  res, err := http.Get(authURL + "/" + user)
+  return err == nil && res.StarusCode == http.StatusOK
+}
+
+
+func testSayHi() {
+  auth = func(string) bool { return true }
+  sayHi("John")
+  
+  auth = func (string) bool { return false }
+  sayHi("John")
+}
+
+
+
+func main() {
+  testSayHi()
+}
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 
