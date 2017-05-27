b2de1200c250e55785fb9c56167a9a769382b366
r00txx <82049406@qq.com>
2017-05-27 15:29:06 +0800

    waitgroup

---
+++ golang_parallel/waitgroup.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+)
+
+func main() {
+    var wg sync.WaitGroup
+    for i := 0; i < 100; i++ {
+        wg.Add(1)
+    }
+
+    for i := 0; i < 100; i++ {
+        go wg.Done()
+    }
+    fmt.Println("exit")
+    wg.Wait()
+}
+
+func add(wg sync.WaitGroup) {
+    wg.Add(1)
+}
+
+
+func done(wg sync.WaitGroup) {
+    wg.Done()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
