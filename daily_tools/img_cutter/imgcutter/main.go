2a56ed7b1ea83a3c633d1592720dee225027373b
r00txx <82049406@qq.com>
2017-08-28 17:11:35 +0800

    add test

--- daily_tools/img_cutter/imgcutter/main.go
+++ daily_tools/img_cutter/imgcutter/main.go
@@ -7,19 +7,30 @@ import (
   "github.com/disintegration/imaging"
 
   "../utils"
+  "os"
+  "strconv"
 )
 
 func main() {
+
+  if len(os.Args) < 3 {
+    fmt.Println("Usage: $1 $2 $3, check readme")
+    os.Exit(1)
+  }
+
   runtime.GOMAXPROCS(runtime.NumCPU())
 
-  img, err := imaging.Open("./big.jpg")
+  img, err := imaging.Open(os.Args[1])
 
 
   utils.CheckErr("img open error", err)
 
-  dstimg := imaging.Resize(img, 200, 0, imaging.Box)
+  width, err := strconv.Atoi(string(os.Args[3]))
+  utils.CheckErr("get with error", err)
 
-  err = imaging.Save(dstimg, "./resized.jpg")
+  dstimg := imaging.Resize(img, width, 0, imaging.Box)
+
+  err = imaging.Save(dstimg, os.Args[2])
 
   utils.CheckErr("resized err", err)
 
