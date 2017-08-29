73f393dfcaf9507ca17abb067580c09e9088fb76
82049406@qq.com <82049406@qq.com>
2017-08-29 17:11:30 +0800

    output anywhere

--- daily_tools/little_scripts_tools/outputAnywhere.go
+++ daily_tools/little_scripts_tools/outputAnywhere.go
@@ -4,16 +4,25 @@ build:  go build outputAnywhere.go
  */
 package main
 
-import "os"
+import (
+	"os"
+	"fmt"
+)
 
 var (
   destFile = "/tmp/outputAnywhere.txt"
 )
 
 func main() {
-  get := []byte(os.Args[1])
-  f, _:= os.OpenFile( destFile, os.O_APPEND|os.O_CREATE, 0777)
-  f.Write(get)
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: string")
+		os.Exit(0)
+	}
+
+	var s string
+  s = string(os.Args[1]) + "\n"
+  f, _:= os.OpenFile(destFile , os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0777)
+  f.WriteString(s)
   f.Close()
 }
 
