8fbef2949d0669a02ebb5059ce6cdd28c6015767
r00txx <82049406@qq.com>
2018-11-07 20:55:12 +0800

    tactx

--- golang_interview/sample.go
+++ golang_interview/sample.go
@@ -5,6 +5,7 @@ import (
 ) 
 
 
+/**
 func main() {
   if (true) {
     defer fmt.Println("1")
@@ -19,3 +20,91 @@ func main() {
   s[0] = 100
   fmt.Println(s)
 }
+**/
+
+
+/**
+func main() {
+  var a Integer = 1
+  var b Integer = 2
+  var i interface{} = &a
+
+  sum := i.(*Integer).Add(b)
+  fmt.Println(sum)
+}
+
+
+
+type Integer int 
+
+func (a Integer) Add (b Integer) Integer {
+  return a + b
+}
+
+
+func (a *Integer) Add(b Integer) Integer {
+  return *a + b
+}
+
+
+type Fragment interface {
+  Exec(transInfo *TransInfo) error
+}
+
+
+type GetPodAction struct {
+
+}
+
+
+func (g GetPodAction) Exec(transInfo *TransInfo) error {
+  // ...
+
+  return nil
+}
+
+
+**/
+
+
+func main() {
+  // i := 1
+  c := complex(3, 12)
+  fmt.Println(c)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
