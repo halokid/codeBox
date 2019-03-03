40c89e2db70d1dd51807b035a36848b83a6d710d
mm jxx <r00txxyyzz@gmail.com>
2019-03-03 12:38:28 +0800

    test code

--- golang_rpc/simple_rpc_test.go
+++ golang_rpc/simple_rpc_test.go
@@ -7,8 +7,7 @@ import (
   "testing"
 )
 
-func TestRPC(t *testing.T) {
-
+func TestRPC(t *testing.T)  {
   gob.Register(User{})
 
   addr := "0.0.0.0:2333"
@@ -20,6 +19,7 @@ func TestRPC(t *testing.T) {
   if err != nil {
     t.Error(err)
   }
+
   cli := NewClient(conn)
 
   var query func(int) (User, error)
@@ -29,23 +29,34 @@ func TestRPC(t *testing.T) {
   if err != nil {
     t.Fatal(err)
   }
+
   fmt.Println(u)
 }
 
+
+
 type User struct {
-  Name string
-  Age  int
+  Name    string
+  Age     int
 }
 
-func queryUser(uid int) (User, error) {
+func queryUser(uid int) (User, error)  {
   userDB := make(map[int]User)
   userDB[0] = User{"Dennis", 70}
-  userDB[1] = User{"Ken", 75}
-  userDB[2] = User{"Rob", 62}
+  userDB[0] = User{"jack", 70}
+  userDB[0] = User{"tom", 70}
+
   if u, ok := userDB[uid]; ok {
     return u, nil
   }
   return User{}, fmt.Errorf("id %d not in user db", uid)
 }
+
+
+
+
+
+
+
 
 
