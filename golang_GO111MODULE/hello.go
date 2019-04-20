cdab10d20b73a7e8c4066e5593c8bed8512eebf7
r0x <r00txxyyzz@gmail.com>
2019-04-20 15:08:51 +0800

    golang package manage

--- golang_GO111MODULE/hello.go
+++ golang_GO111MODULE/hello.go
@@ -1,10 +1,27 @@
 package main
 
-import "github.com/astaxie/beego"
+import (
+  "hello/utils"
 
+  "fmt"
+  "github.com/mattbaird/jsonpatch"
+) 
+
+
+var simpleA = `{"a":100, "b":200, "c":"hello"}`
+var simpleB = `{"a":100, "b":200, "c":"goodbye"}`
 
 func main() {
-  beego.Run()
+    patch, e := jsonpatch.CreatePatch([]byte(simpleA), []byte(simpleB))
+    if e != nil {
+        fmt.Printf("Error creating JSON patch:%v", e)
+        return
+    }
+    for _, operation := range patch {
+        fmt.Printf("%s\n", operation.Json())
+    }
+
+    utils.PrintText("Hi")
 }
 
 
---
+++ golang_GO111MODULE/utils/tools.go
@@ -0,0 +1,8 @@
+package utils
+
+import "fmt"
+
+func PrintText(txt string) {
+  fmt.Println(txt) 
+}
+
