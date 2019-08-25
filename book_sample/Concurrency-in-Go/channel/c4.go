ee4c11231bdb39f04ec60e079dfe3bd41b1e454f
mm jxx <r00txxyyzz@gmail.com>
2019-08-25 16:21:49 +0800

    channel sample

---
+++ book_sample/Concurrency-in-Go/channel/c4.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+  "bytes"
+  "fmt"
+  "os"
+)
+
+func main() {
+  var stdoutBuff bytes.Buffer
+  defer stdoutBuff.WriteTo(os.Stdout)
+
+  intStream := make(chan int, 4)
+
+  go func() {
+    defer close(intStream)
+    defer println(&stdoutBuff, "Producer Done")
+
+    for i := 0; i < 5; i++ {
+      fmt.Fprintf(&stdoutBuff, "sending: %d\n", i)
+      intStream <- i
+    }
+  }()
+
+  for integer := range intStream {
+    fmt.Fprintf(&stdoutBuff, "received %v\n", integer)
+  }
+}
+
+
+
+
+
+
