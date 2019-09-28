a6e2efa6afa58b0071dabf89b7e2993cffdea3ec
mm jxx <r00txxyyzz@gmail.com>
2019-09-28 21:12:34 +0800

    sample

--- golang_basic/test.go
+++ golang_basic/test.go
@@ -1,12 +1,27 @@
 package main
 
-import (
-  "fmt"
-)
+func foox(sl []int) {
+  sl[0] = 9
+}
+
+func fooy() string {
+  defer println("fooy defer")
+  panic("fooy panic")
+  return "fooy func"
+}
 
 func main() {
-	i := 7
-	k := i >> 1
-	fmt.Println(k)
+  /**
+  sl := make([]int, 10)
+  fmt.Println(reflect.TypeOf(sl))
+
+  sl = append(sl, 1)
+  fmt.Println(sl)
+
+  foox(sl)
+  fmt.Println(sl)
+  */
+
+  fooy()
 }
 
