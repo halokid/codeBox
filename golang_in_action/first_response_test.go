e4e30b00a3bb03f74afddd79e5c8aecf870d99c4
mm jxx <r00txxyyzz@gmail.com>
2020-01-05 18:18:53 +0800

    channel first response

--- golang_in_action/first_response_test.go
+++ golang_in_action/first_response_test.go
@@ -2,13 +2,59 @@ package golang_in_action
 
 import (
   "fmt"
+  "github.com/jinzhu/gorm"
+  _ "github.com/jinzhu/gorm/dialects/mysql"
+  "github.com/spf13/cast"
+  "math/rand"
   "runtime"
   "testing"
   "time"
 )
 
+
+var Db *gorm.DB
+
+func init()  {
+  var err error
+  Db, err = gorm.Open("mysql",  "root:mysql-pw@tcp(192.168.1.129:33061)/ocx?charset=utf8&parseTime=True&loc=Local")
+  if err != nil {
+    panic(err)
+  }
+}
+
+type Deploy struct {
+  ID      int
+  Name    string
+}
+
+func (d Deploy) TableName() string {
+  return "deploy"
+}
+
+func GetDeploys() []Deploy {
+  // 获取所有deploy信息
+  var deploys []Deploy
+  Db.Find(&deploys)
+  return deploys
+}
+
+func AddDeploy(d *Deploy) error {
+  // 验证 FirstResponse这个父协程退出(return)之后， 子协程runTask还会不会执行?
+  if err := Db.Create(d).Error; err != nil {
+    return err
+  }
+  return nil
+}
+
+
 func runTask(id int) string {
-  time.Sleep(10 * time.Second)
+  //rand.Seed(time.Now().Unix())
+  randI := rand.Intn(30)
+  fmt.Println("睡眠 ", randI, "验证下FirstResponse函数这个协程已经摧毁了之后，其他的gor还会不会跑这个addDb")
+  time.Sleep(time.Duration(randI) * time.Second)
+  //time.Sleep(10 * time.Second)
+  d := Deploy{Name: "xxxx" + cast.ToString(randI)}
+  AddDeploy(&d)
   return fmt.Sprintf("the result is from %d", id)
 }
 
@@ -25,6 +71,8 @@ func FirstResponse() string {
       ch <-ret
     }(i)
   }
+  // fixme:
+  time.Sleep(3 * time.Second)
   return <-ch
 }
 
@@ -32,7 +80,9 @@ func TestFirstResponse(t *testing.T) {
   // fixme: 用了 buffer channel之后， 这里会输出2 ，所以证明必要一个channel 返回， 其他的gor都会退出，2是最少的协程数量， 因为main func一个，  返回的gor一个， 一共2个
   t.Log("Before:", runtime.NumGoroutine())
   t.Log(FirstResponse())
-  time.Sleep(1 * time.Second)
+  //time.Sleep(3 * time.Second)
+  time.Sleep(15 * time.Second)
+  fmt.Println("总的main gor等待 15秒, 调用的函数等待 3 秒, 则cancel掉的协程只要执行时间不超过18秒的都会执行 AddDeploy 函数")
   t.Log("After:", runtime.NumGoroutine())
   t.Log("before  和  after 的协程数是一样的， 证明没有协程泄漏, 也就是说没有往close的 channel写东西， 也没有协程在做无意义的等待")
 }
