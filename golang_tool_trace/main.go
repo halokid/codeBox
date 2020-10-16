9904e41d1baafac5fdd4cc2c6aec1b5672d75bba
r0x <r00txxyyzz@gmail.com>
2020-10-16 11:22:35 +0800

    go tool trace

---
+++ golang_tool_trace/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "os"
+  "runtime/trace"
+)
+
+func main() {
+  trace.Start(os.Stderr)
+  defer trace.Stop()
+
+  ch := make(chan string)
+  go func() {
+    ch <-"EDDYCJY"
+  }()
+
+  <-ch
+}
+
+
+
