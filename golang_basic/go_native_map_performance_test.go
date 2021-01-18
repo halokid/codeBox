6e86c024f3b0ef4d04efab687b9aa9e2ff9d824f
r0x <r00txxyyzz@gmail.com>
2021-01-18 15:41:28 +0800

    native map performance

--- golang_basic/go_native_map_performance_test.go
+++ golang_basic/go_native_map_performance_test.go
@@ -8,6 +8,12 @@ func test(m map[int]int) {
   }
 }
 
+func testx(m map[int]struct{}) {
+  for i := 0; i < 10000; i++ {
+    m[i] = struct{}{}
+  }
+}
+
 func BenchmarkMap(b *testing.B) {
   for i := 0; i < b.N; i++ {
     b.StopTimer()
@@ -25,4 +31,14 @@ func BenchmarkMapCap(b *testing.B) {
     test(m)
   }
 }
+
+func BenchmarkMapCap2(b *testing.B) {
+  for i := 0; i < b.N; i++ {
+    b.StopTimer()
+    m := make(map[int]struct{}, 10000)
+    b.StartTimer()
+    testx(m)
+  }
+}
+
 
