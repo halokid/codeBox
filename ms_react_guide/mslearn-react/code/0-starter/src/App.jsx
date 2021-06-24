199f7370212239a6a6d193ccaa5b5d8e0e8321a2
mm jxx <r00txxyyzz@gmail.com>
2021-06-25 00:01:19 +0800

    doing

---
+++ ms_react_guide/mslearn-react/code/0-starter/src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import RecipeTitle from "./RecipeTitle";
+
+function App() {
+  return (
+    <article>
+      <h1>Recipe Manager</h1>
+      <RecipeTitle />
+    </article>
+  )
+}
+
+export default App;
+
+
--- ms_react_guide/mslearn-react/code/0-starter/src/index.jsx
+++ ms_react_guide/mslearn-react/code/0-starter/src/index.jsx
@@ -1,9 +1,13 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./App";
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  // <h1>Hello, world!</h1>,
+  // document.getElementById('app')
+
+  <App />,
   document.getElementById('app')
 );
 
