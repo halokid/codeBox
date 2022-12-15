8db3b506b08f31cdc6d051e346458207d1f7bb00
mm jxx <r00txxyyzz@gmail.com>
2022-12-15 20:05:15 +0800

    springcloud e-commerce project

--- ecommercespringcloud/e-commerce-authority-center/src/main/java/org/halokid/AuthorityCenterApplication.java
+++ ecommercespringcloud/e-commerce-authority-center/src/main/java/org/halokid/AuthorityCenterApplication.java
@@ -7,9 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * <h1>授权中心启动入口</h1>
- * Create by tachai on 2021/12/29 5:36 下午
- * gitHub https://github.com/TACHAI
- * Email tc1206966083@gmail.com
  */
 @EnableJpaAuditing    // 允许jpa 自动审计
 @EnableDiscoveryClient
