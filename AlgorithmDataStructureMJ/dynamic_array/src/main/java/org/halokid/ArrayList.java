23c57341f1dc24a476f21f047a8410242e6d79e4
mm jxx <r00txxyyzz@gmail.com>
2023-06-13 23:41:18 +0800

    coding dynamic array

---
+++ AlgorithmDataStructureMJ/dynamic_array/src/main/java/org/halokid/ArrayList.java
@@ -0,0 +1,54 @@
+package org.halokid;
+
+public class ArrayList {
+
+  public void clear() {
+
+  }
+
+  public int size() {
+    return 0;
+  }
+
+  public boolean isEmpty() {
+    return false;
+  }
+
+  public boolean contains(int element) {
+    return false;
+  }
+
+  public void add(int element) {
+
+  }
+
+  public void add(int index, int element) {
+
+  }
+
+  public int get(int index) {
+    return 0;
+  }
+
+  public int set(int index, int element) {
+    return 0;
+  }
+
+  public int remove(int index) {
+    return 0;
+  }
+
+  public int indexOf(int element) {
+    return -1;
+  }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ AlgorithmDataStructureMJ/dynamic_array/src/main/java/org/halokid/Main.java
@@ -0,0 +1,13 @@
+package org.halokid;
+
+public class Main {
+  public static void main(String[] args) {
+
+    int array[] = new int[] {11, 22, 33};
+
+    ArrayList list = new ArrayList();
+    list.add(11);
+    list.add(0, 22);
+    list.remove(1);
+  }
+}
